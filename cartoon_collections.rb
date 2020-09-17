5488e9a33177ca4607522cc3515a4440eca34e82
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-17 22:10:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each  do 
+  array.each  do |character|
     puts "Hello " + character +"!"
   end
 end
